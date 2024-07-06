af2a035b2b16048e1e9426974d86cd53e399dab0
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-06 21:20:33 +0100

    urls set up

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('auth/register', UserRegistrationView.as_view(), name='register'),
+    path('auth/login', UserLoginView.as_view(), name='login'),
+    path('api/users/<str:pk>', UserDetailView.as_view(), name='user-detail'),
+    path('api/organisations', OrganizationListView.as_view(), name='organisation-list'),
+    path('api/organisations/<str:pk>', OrganizationDetailsView.as_view(), name='organisation-detail'),
+    path('api/organisations/create', OrganizationCreateView.as_view(), name='organisation-create'),
+    path('api/organisations/<str:org_id>/users', AddUserToOrganizationView.as_view(), name='add-user-to-org'),
+]
