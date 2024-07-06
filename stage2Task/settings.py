4e5bad09d6d2e5a0a63eadee1785b4a3e5c08038
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-06 22:43:09 +0100

    debug

--- stage2Task/settings.py
+++ stage2Task/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -50,6 +50,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'USER_ID_FIELD': 'user_id',
 }
 
