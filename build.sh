6937500e3a0faf295388e5bb4ca2ce035da60100
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-07 23:40:07 +0100

    deployment

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 echo "  BUILD START"
-
-# Activate virtual environment
-source venv/bin/activate
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
-python3.12 manage.py collectstatic --noinput --clear
-
+python3 manage.py collectstatic --noinput --clear
 echo "  BUILD END"
