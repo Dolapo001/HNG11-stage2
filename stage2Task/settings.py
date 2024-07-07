4eb546ee581756d1d5e0716336da54080c10c4f4
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-07 22:07:32 +0100

    deployment

--- stage2Task/settings.py
+++ stage2Task/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@j18qwb+_9rw44k@_1ny)w4+^glojw-)i6#o%ped@tdlppel@k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
