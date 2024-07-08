84103a27ea4d849ffa2ba6aa35b0d69fe1385e26
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-08 10:45:52 +0100

    changes

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
-echo "BUILD START"
+#!/bin/bash 
+
+echo "  BUILD START"
+
 source venv/bin/activate
-pip install --upgrade pip  # Ensure pip is up to date
-pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
-echo "BUILD END"
+
+pip3 install -r requirements.txt
+
+python3.12 manage.py collectstatic --noinput 
+
+echo "  BUILD END"
