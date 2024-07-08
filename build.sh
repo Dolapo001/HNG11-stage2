e5860ec3a4f80273075542b7d2c9251f62eae1e4
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-08 09:51:19 +0100

    changes

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
-echo "  BUILD START"
+#!/bin/bash
+echo "BUILD START"
+
+# Ensure pip is installed
+python3 -m ensurepip --upgrade
+
+# Install virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python3 manage.py collectstatic --noinput --clear
-echo "  BUILD END"
+
+echo "BUILD END"
--- stage2Task/settings.py
+++ stage2Task/settings.py
@@ -151,13 +151,9 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
