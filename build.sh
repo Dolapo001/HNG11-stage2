c45dbf0a03af2c6532aa94a8873b4112c87df75e
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-08 10:09:03 +0100

    changes

--- build.sh
+++ build.sh
@@ -1,17 +1,5 @@
-#!/bin/bash
 echo "BUILD START"
-
-# Ensure pip is installed
-python3 -m ensurepip --upgrade
-
-# Install virtual environment
-python3 -m venv venv
 source venv/bin/activate
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
-python3 manage.py collectstatic --noinput --clear
-
+python manage.py collectstatic --noinput --clear
 echo "BUILD END"
