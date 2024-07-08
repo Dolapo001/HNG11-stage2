a8ce5ea0e2abbff19f64899a8b14b7e43220a0ee
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-08 10:25:14 +0100

    changes

--- stage2Task/settings.py
+++ stage2Task/settings.py
@@ -154,9 +154,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
